330aa11fa8f0baf19ae0799505b29b29e3556efb
Jason Crawford <jason@kimalabs.com>
2011-12-06 21:41:35 -0800

    Fresh Rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreeBooks::Application.config.secret_token = 'ab321b88c655d32b7d349a5da0cf0ab74f0049a9f304b1189f4b02a23056440254cba70d4defdc79885e9c6b9b632fc33d618d46796d2b4e13f8733b7bf10eb8'
