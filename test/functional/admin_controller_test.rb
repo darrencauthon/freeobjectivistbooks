c992044a8583e0131bf7a773f3148fe24bbb5133
Jason Crawford <jason@kimalabs.com>
2012-01-07 21:34:44 -0800

    Fix test (again)

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -5,8 +5,8 @@ class AdminControllerTest < ActionController::TestCase
     digest_auth "admin", "admin", "Admin", "password"
     get :index
     assert_response :success
-    assert_select '.request p.summary', /Howard Roark (roark@stanton.edu) wants Atlas Shrugged/
-    assert_select '.pledge p.summary', /Hugh Akston (akston@patrickhenry.edu) pledged 5 books/
+    assert_select '.request p.summary', "Howard Roark (roark@stanton.edu) wants Atlas Shrugged"
+    assert_select '.pledge p.summary', "Hugh Akston (akston@patrickhenry.edu) pledged 5 books"
   end
 
   test "admin password is required" do
