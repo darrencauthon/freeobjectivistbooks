1c3090d3f47587672737d51abec552a471d18620
Jason Crawford <jason@kimalabs.com>
2012-01-10 01:44:10 -0800

    More stats on admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,8 +6,13 @@ class AdminController < ApplicationController
   end
 
   def index
-    @request_total = Request.count
-    @pledge_total = Pledge.sum :quantity
+    @request_count = Request.count
+    @open_request_count = Request.open.count
+    @granted_request_count = Request.granted.count
+
+    @pledge_count = Pledge.count
+    @pledge_quantity = Pledge.sum :quantity
+
     @requests = Request.includes(:user).order('created_at desc')
     @pledges = Pledge.includes(:user).order('created_at desc')
   end
