3f3dbecc554e09413f4ba4b22dadc7f82251a94c
Jason Crawford <jason@kimalabs.com>
2012-01-25 19:41:30 -0800

    Use a layout for event notifications

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -3,28 +3,33 @@ class EventMailer < ApplicationMailer
     self.send "#{event.type}_event", event
   end
 
+  def notification(subject)
+    mail_to_user @event.to, subject: subject
+  end
+
   def grant_event(event)
     @event = event
-    mail_to_user @event.to, subject: "We found a donor to send you #{@event.request.book}!"
+    @closer = "Happy reading"
+    notification "We found a donor to send you #{@event.request.book}!"
   end
 
   def flag_event(event)
     @event = event
-    mail_to_user @event.to, subject: "Problem with your shipping info on Free Objectivist Books"
+    notification "Problem with your shipping info on Free Objectivist Books"
   end
 
   def update_event(event)
     @event = event
-    mail_to_user @event.to, subject: "#{@event.user.name} #{@event.detail} on Free Objectivist Books"
+    notification "#{@event.user.name} #{@event.detail} on Free Objectivist Books"
   end
 
   def message_event(event)
     @event = event
-    mail_to_user @event.to, subject: "#{@event.user.name} sent you a message on Free Objectivist Books"
+    notification "#{@event.user.name} sent you a message on Free Objectivist Books"
   end
 
   def thank_event(event)
     @event = event
-    mail_to_user @event.to, subject: "#{@event.user.name} sent you a thank-you note for #{@event.request.book}"
+    notification "#{@event.user.name} sent you a thank-you note for #{@event.request.book}"
   end
 end
