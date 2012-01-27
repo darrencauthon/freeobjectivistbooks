e30c65bb4f9e4bf3202e562308a8302c0c6d7291
Jason Crawford <jason@kimalabs.com>
2012-01-27 00:56:09 -0800

    Add an admin request detail view that shows all the events for a request

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,4 +1,8 @@
 class Admin::RequestsController < AdminController
+  def load_models
+    @request = Request.find params[:id] if params[:id]
+  end
+
   def index
     @request_count = Request.count
     @open_request_count = Request.open.count
