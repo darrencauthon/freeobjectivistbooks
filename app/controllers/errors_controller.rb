bc1a1b1449493fd302a85375a63f8f65eb1dca1b
Jason Crawford <jason@kimalabs.com>
2012-01-27 00:07:46 -0800

    Render empty file on any not-found URL that isn't HTML (XML, CSS, text, etc.)

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,8 @@
 class ErrorsController < ApplicationController
   def not_found
-    render '404', status: :not_found
+    respond_to do |format|
+      format.html { render '404', status: :not_found }
+      format.any { render nothing: true, status: :not_found }
+    end
   end
 end
---
+++ test/integration/errors_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ErrorsTest < ActionDispatch::IntegrationTest
+  fixtures :all
+
+  test "not found" do
+    get "/does/not/exist"
+    assert_response :not_found
+    assert_select 'h1', /not found/i
+  end
+
+  test "server error" do
+    get "/barf"
+    assert_response :internal_server_error
+    assert_select 'h1', /error/i
+  end
+
+  test "not found XML" do
+    get "/crossdomain.xml"
+    assert_response :not_found
+  end
+end
