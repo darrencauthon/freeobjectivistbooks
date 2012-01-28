5257c4cf609e52fbdefb29859178dd0ddc3fc526
Jason Crawford <jason@kimalabs.com>
2012-01-27 18:20:23 -0800

    Use event.donor, not event.request.donor (just in case request donor has been updated and they are now different)

--- app/mailers/announcement_mailer.rb
+++ app/mailers/announcement_mailer.rb
@@ -21,7 +21,7 @@ class AnnouncementMailer < ApplicationMailer
 
   def reply_to_thanks(event)
     @event = event
-    @user = @event.request.donor
+    @user = @event.donor
     announcement "Now you can reply to #{@event.from.name}'s thank-you note on Free Objectivist Books"
   end
 end
