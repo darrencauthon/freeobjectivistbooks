61539d0bf68297e64d0e6852d71a1311778bafeb
Jason Crawford <jason@kimalabs.com>
2012-01-31 03:13:50 -0800

    Fix migration for PostgreSQL

--- db/migrate/20120127222929_add_status_to_requests.rb
+++ db/migrate/20120127222929_add_status_to_requests.rb
@@ -1,6 +1,6 @@
 class AddStatusToRequests < ActiveRecord::Migration
   def change
     add_column :requests, :status, :string
-    execute 'update requests set status="not_sent" where donor_id is not null'
+    execute "update requests set status='not_sent' where donor_id is not null"
   end
 end
