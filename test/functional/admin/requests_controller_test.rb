0a10950bad9f89266f9e19d426d88d7e1a1b754e
Jason Crawford <jason@kimalabs.com>
2012-01-31 16:32:29 -0800

    Show correct request status in admin view

--- test/functional/admin/requests_controller_test.rb
+++ test/functional/admin/requests_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class Admin::RequestsControllerTest < ActionController::TestCase
+  # Index
+
   test "index" do
     admin_auth
     get :index
@@ -9,16 +11,40 @@ class Admin::RequestsControllerTest < ActionController::TestCase
     assert_select '.request', Request.count
   end
 
-  test "show" do
-    authenticate_with_http_digest "admin", "password", "Admin"
-    get :show, id: @howard_request.id
-    assert_response :success
-    assert_select 'h1', /Howard Roark wants\s+Atlas Shrugged/
-  end
-
   test "index requires login" do
     get :index
     assert_response :unauthorized
     assert_select '.request', 0
+  end
+
+  # Show
+
+  test "show no donor" do
+    admin_auth
+    get :show, id: @howard_request.id
+    assert_response :success
+    assert_select 'h1', /Howard Roark wants\s+Atlas Shrugged/
+    assert_select 'h2', /looking for donor/i
+  end
+
+  test "show with donor" do
+    admin_auth
+    get :show, id: @hank_request.id
+    assert_response :success
+    assert_select 'h1', /Hank Rearden wants\s+Atlas Shrugged/
+    assert_select 'h2', /donor found/i
+  end
+
+  test "show with book sent" do
+    admin_auth
+    get :show, id: @quentin_request.id
+    assert_response :success
+    assert_select 'h1', /Quentin Daniels wants\s+The Virtue of Selfishness/
+    assert_select 'h2', /book sent/i
+  end
+
+  test "show requires login" do
+    get :show, id: @howard_request.id
+    assert_response :unauthorized
   end
 end
