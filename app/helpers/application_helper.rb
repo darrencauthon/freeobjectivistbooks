2f1c92b201cb71521305f0251b325cfe812e9c27
Jason Crawford <jason@kimalabs.com>
2012-02-01 10:21:08 -0800

    Fix number display to use all integer digits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,20 @@ module ApplicationHelper
     address.present? ? format_block(address) : "No address given"
   end
 
+  def count_digits(number)
+    if number == 0
+      1
+    elsif number < 0
+      digits -number
+    else
+      1 + Math.log10(number).floor
+    end
+  end
+
   def format_number(number, precision = 2)
-    number_with_precision number, precision: precision, significant: true, strip_insignificant_zeros: true
+    digits = count_digits number
+    precision = digits if precision < digits
+    number_with_precision number, precision: precision, significant: true, strip_insignificant_zeros: true, delimiter: ","
   end
 
   def pluralize_omit_number(count, noun)
--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -21,4 +21,24 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal "apple", pluralize_omit_1(1, "apple")
     assert_equal "2 apples", pluralize_omit_1(2, "apple")
   end
+
+  test "format number" do
+    assert_equal "0", format_number(0)
+    assert_equal "0.5", format_number(0.49876)
+    assert_equal "0.5", format_number(0.5)
+    assert_equal "0.54", format_number(0.54321)
+    assert_equal "1", format_number(1)
+    assert_equal "9", format_number(9)
+    assert_equal "9.9", format_number(9.949)
+    assert_equal "10", format_number(9.95)
+    assert_equal "10", format_number(10)
+    assert_equal "10", format_number(10.3)
+    assert_equal "11", format_number(11)
+    assert_equal "99", format_number(99)
+    assert_equal "100", format_number(100)
+    assert_equal "100", format_number(100.3)
+    assert_equal "101", format_number(101)
+    assert_equal "999", format_number(999)
+    assert_equal "1,000", format_number(1000)
+  end
 end
