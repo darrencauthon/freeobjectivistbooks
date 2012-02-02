c0318058515a64ca871bef55c44cf5b0d34cd4fd
Jason Crawford <jason@kimalabs.com>
2012-02-02 01:34:26 -0600

    Add a ReminderMailer layout to get salutation and closing

--- test/functional/reminder_mailer_test.rb
+++ test/functional/reminder_mailer_test.rb
@@ -17,10 +17,12 @@ class ReminderMailerTest < ActionMailer::TestCase
 
     mail.deliver
     assert_select_email do
+      assert_select 'p', /Hi Hugh/
       assert_select 'p', /Thank you for\s+donating 3 books so far/
       assert_select 'p', /On Jan 15, you pledged to donate 5 books/
       assert_select 'p', /Right now there are 2 students waiting/
       assert_select 'a', /Read their appeals/
+      assert_select 'p', /Thanks,\nFree Objectivist Books/
     end
   end
 
@@ -31,10 +33,12 @@ class ReminderMailerTest < ActionMailer::TestCase
 
     mail.deliver
     assert_select_email do
+      assert_select 'p', /Hi Robert/
       assert_select 'p', /Thank you for\s+signing up to donate books/
       assert_select 'p', /On Jan 17, you pledged to donate 3 books/
       assert_select 'p', /Right now there are 2 students waiting/
       assert_select 'a', /Read their appeals/
+      assert_select 'p', /Thanks,\nFree Objectivist Books/
     end
   end
 end
