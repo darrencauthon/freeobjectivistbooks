a50c7cbee522c476b0f358a4b4042f1a7d2acc0d
Jason Crawford <jason@jasoncrawford.org>
2012-04-15 23:54:39 -0700

    Add more reports to drill into which requests are stuck in the pipeline

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,6 +1,18 @@
 class Admin::RequestsController < AdminController
   def index
     @metrics = Metrics.new
-    @requests = limit_and_offset Request
+
+    @requests = case params[:type]
+    when 'not_granted'
+      Request.not_granted.reorder(:created_at)
+    when 'needs_sending'
+      Donation.needs_sending.reorder(:created_at).map {|d| d.request}
+    when 'in_transit'
+      Donation.in_transit.reorder(:status_updated_at).map {|d| d.request}
+    when 'reading'
+      Donation.reading.reorder(:status_updated_at).map {|d| d.request}
+    else
+      limit_and_offset Request
+    end
   end
 end
