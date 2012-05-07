7656c44eea4945676d3ba705194dd407a6635516
Jason Crawford <jason@jasoncrawford.org>
2012-05-06 18:19:46 -0700

    Destroy ReminderEntities when parent Reminder is destroyed

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -2,7 +2,7 @@ class Reminder < ActiveRecord::Base
   self.inheritance_column = 'class'  # anything other than "type", to let us use "type" for something else
 
   belongs_to :user
-  has_many :reminder_entities
+  has_many :reminder_entities, dependent: :destroy
   has_many :pledges, through: :reminder_entities, source: :entity, source_type: 'Pledge'
   has_many :donations, through: :reminder_entities, source: :entity, source_type: 'Donation'
 end
