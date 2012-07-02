45ad4e4750f745a1c6bb7c521068a745ae071833
Jason Crawford <jason@jasoncrawford.org>
2012-07-01 22:46:15 -0700

    Map tweaks: * Count all countries, not just localities * List the countries on the page * Nudge the map over so New Zealand shows

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,8 @@ class LocationsController < ApplicationController
   def index
     @users = Request.active.includes(:user).all.map {|request| request.user}.uniq
     @location_names = @users.map {|user| user.location}.uniq
-    @locations = Location.where(name: @location_names).select {|location| location.locality?}
-    @countries = @locations.map {|location| location.country}.uniq
+    @locations = Location.where(name: @location_names)
+    @countries = @locations.map {|location| location.country}.uniq.sort
+    @markers = @locations.select {|location| location.locality?}
   end
 end
