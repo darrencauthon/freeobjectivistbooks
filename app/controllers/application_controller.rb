bb37e9c1c87dd9cf09507d9e7a53a0d0992a1992
Jason Crawford <jason@jasoncrawford.org>
2012-09-15 17:16:04 -0700

    Add some documentation to the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,18 @@ class ApplicationController < ActionController::Base
 
   def initialize
     super
+    # Introspects the relevant model for this controller. E.g., if this is the RequestsController,
+    # the model is Request and the ivar is @request. This is used by load_models.
     @model_class_name = self.class.name.split("::").last.sub(/Controller$/,"").singularize
     @model_class = @model_class_name.constantize if Kernel.const_defined?(@model_class_name)
     @model_ivar_name = "@#{@model_class_name.underscore}"
   end
 
+  #--
   # Filters
+  #++
 
+  # Loads the current user from the session. Automatically invoked before all requests.
   def find_current_user
     if session[:user_id]
       @current_user = User.find_by_id session[:user_id]
@@ -26,6 +31,7 @@ class ApplicationController < ActionController::Base
     logger.info "current user: " + (@current_user ? "#{@current_user.name} (#{@current_user.id})" : "none")
   end
 
+  # Sets the current user in the session.
   def set_current_user(user)
     logger.info "setting current user: " + (@current_user ? "#{@current_user.name} (#{@current_user.id})" : "none")
     reset_session
@@ -33,8 +39,8 @@ class ApplicationController < ActionController::Base
     @current_user = user
   end
 
-  # Creates a Referral object to track this referral/landing; stores it in the
-  # session so it can be associated with any downstream signup.
+  # Creates a Referral object to track this referral/landing; stores it in the session so it can be
+  # associated with any downstream signup.
   def store_referral
     return unless params[:utm_source] || params[:utm_medium]
     referral = Referral.create source: params[:utm_source], medium: params[:utm_medium], landing_url: request.url,
@@ -42,15 +48,25 @@ class ApplicationController < ActionController::Base
     session[:referral_id] = referral.id
   end
 
+  # By convention, extracts values from params, parses them if needed, and stores them in ivars, e.g.:
+  #
+  #     @public = params[:public].to_bool
+  #
+  # Default implementation is empty; subclasses can override. Called as a before_filter on all requests.
   def parse_params
   end
 
+  # Loads the main model, if any, from the id parameter, along with some other models commonly
+  # used by controllers. May be overridden by subclasses to load additional models, but subclass
+  # implementations should call super.
   def load_models
     instance_variable_set @model_ivar_name, @model_class.find(params[:id]) if @model_class && params[:id]
     @request = Request.find params[:request_id] if params[:request_id]
     @donation = Donation.find params[:donation_id] if params[:donation_id]
   end
 
+  # Invokes render_unauthorized if there is no current logged-in user.
+  # Optional before_filter that subclasses can use. Has the effect
   def require_login
     raise UnauthorizedException if !@current_user
   end
@@ -60,17 +76,28 @@ class ApplicationController < ActionController::Base
     users = users.flatten.compact
     raise ForbiddenException if !@current_user.in?(users)
   end
+  private :require_user
 
+  # Specifies who can access this page. Subclasses can override this to return a user or a list of
+  # users, in order to restrict access to those users. The render_forbidden handler will be invoked
+  # for all other users.
+  #
+  # If nil or empty list is returned, *all* users are allowed. (This is the default.)
   def allowed_users
   end
 
+  # Invokes render_forbidden if there are allowed_users and the current user is not one of them.
+  # Automatically invoked as a before_filter on all requests.
   def check_user
     users = Array(allowed_users)
     require_user users unless users.empty?
   end
 
+  #--
   # Helpers
+  #++
 
+  # Saves a set of models. Validates *all* models before saving *any* of them.
   def save(*models)
     models = models.flatten.compact
     valid = models.all? {|m| m.valid?}
@@ -83,6 +110,7 @@ class ApplicationController < ActionController::Base
       logger.warn "#{model.class} errors: #{model.errors.messages}" if model.invalid?
     end
   end
+  private :log_errors
 
   def limit_and_offset(relation, default_limit = 100)
     offset = params[:offset]
@@ -100,7 +128,9 @@ class ApplicationController < ActionController::Base
     @all
   end
 
+  #--
   # Error handling
+  #++
 
   unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: :render_error
@@ -113,6 +143,7 @@ class ApplicationController < ActionController::Base
   rescue_from UnauthorizedException, with: :render_unauthorized
   rescue_from ForbiddenException, with: :render_forbidden
 
+  # Renders an HTTP 401 Unauthorized response for HTML or JSON.
   def render_unauthorized
     respond_to do |format|
       format.html do
@@ -126,6 +157,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # Renders an HTTP 403 Forbidden response for HTML or JSON.
   def render_forbidden
     respond_to do |format|
       format.html do
@@ -138,6 +170,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # Renders an HTTP 404 Not Found response for HTML or JSON.
   def render_not_found
     respond_to do |format|
       format.html { render template: "errors/404", status: 404 }
@@ -145,6 +178,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # Renders an HTTP 500 Internal Server Error response for HTML or JSON. Invoked on all uncaught exceptions.
   def render_error(exception)
     trace = exception.backtrace.map {|frame| "    #{frame}"}.join("\n")
     logger.error "Caught exception #{exception.class}: #{exception.message}\n#{trace}"
